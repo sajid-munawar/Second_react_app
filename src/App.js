95510fe595e33d9e7a9393550f43faa494688185
sajid507 <sajid92500@gmail.com>
2020-10-12 00:13:48 +0500

    created second react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {Message} from "./Message.js"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  let [count , setCount]= useState(1)
+  return <div> 
+       <Message counter={count} />
+    <button onClick={ ()=> setCount(count +1)} >increment</button>
     </div>
-  );
 }
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export function Message (props) {
+      return <div> 
+          The value of counter is {props.counter} 
+          </div>
+}
