9f786d2c926e389cb95b6886ef04e5dcb1719474
sajid507 <sajid92500@gmail.com>
2020-10-12 14:14:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import {Message} from "./Message.js"
 import "./App.css"
 
 function App() {
-  // let [isDay,setDay]=useState(true)
+  let [isMoring,setMorning]=useState(true)
   let [count , setCount]= useState(1)
   return <div className="box"> 
+      <h1>Is Moring {isMoring} ? "Moring":"Night" </h1>
        <Message counter={count} />
     <button onClick={ ()=> setCount(count +1)} >increment</button>
     </div>
