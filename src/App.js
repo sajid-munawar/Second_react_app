8a658c1c92f179424a52020e5c760472637c289a
sajid507 <sajid92500@gmail.com>
2020-10-12 13:49:22 +0500

    added app.css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import './App.css';
 import { useState } from 'react';
 import {Message} from "./Message.js"
+import "./App.css"
 
 function App() {
-  
+  let [isDay,setDay]=useState(true)
   let [count , setCount]= useState(1)
-  return <div> 
+  return <div className="box"> 
        <Message counter={count} />
     <button onClick={ ()=> setCount(count +1)} >increment</button>
     </div>
